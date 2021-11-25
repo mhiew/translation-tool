c981bb89ff3b0dc3a3824abf4dfb39029c0c80f4
Min Hiew <mhiew@nudgerewards.com>
2021-11-25 12:54:58 -0500

    Initial Blank Application

---
+++ build.gradle.kts
@@ -0,0 +1,23 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.6.0"
+}
+
+group = "com.minhiew"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnit()
+}
+
+tasks.withType<KotlinCompile>() {
+    kotlinOptions.jvmTarget = "1.8"
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,3 @@
+
+rootProject.name = "translation-tool"
+
