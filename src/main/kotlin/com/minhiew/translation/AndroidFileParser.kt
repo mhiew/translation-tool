5a3b76caf49bad1a5de72de04bcf6441ee1dbe39
Min Hiew <mhiew@nudgerewards.com>
2021-11-25 19:06:21 -0500

    Update The Android File Parsing to use and XML Parser
    - remove regex parsing of xml
    - switch to dom4j to parse xml so that we can support multiline xml tags

--- src/main/kotlin/com/minhiew/translation/AndroidFileParser.kt
+++ src/main/kotlin/com/minhiew/translation/AndroidFileParser.kt
@@ -1,5 +1,6 @@
 package com.minhiew.translation
 
+import org.dom4j.io.SAXReader
 import java.io.BufferedReader
 import java.io.File
 import java.io.StringReader
@@ -10,19 +11,13 @@ object AndroidFileParser {
     fun parse(fileContents: String): Map<String, String> = parse(BufferedReader(StringReader(fileContents)))
 
     fun parse(fileContents: BufferedReader): Map<String, String> {
-        val result = mutableMapOf<String, String>()
-        fileContents.use {
-            it.forEachLine { line ->
-                if (line.isNotBlank()) {
-                    "<string.+?name=\"(.+?)\".*?>(.+?)</.+?>".toRegex().findAll(line).forEach {
-                        val key = it.groupValues[1]
-                        val value = it.groupValues[2]
-                        result[key] = value
-                    }
-                }
-            }
-        }
+        val document = SAXReader().read(fileContents)
+        val stringElements = document.rootElement.elements("string")
 
-        return result
+        return stringElements
+            .filterNot { it.text.isNullOrBlank() }
+            .associate { element ->
+                element.attributeValue("name") to element.text.trim()
+            }
     }
 }
